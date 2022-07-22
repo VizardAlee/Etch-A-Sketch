06dbaf6a95420e1b91b02f7b2dc62fb8d9da093b
VizardAlee <biggy0023@gmail.com>
2022-07-22 11:37:01 +0100

    With further study, drastic changes had to be made to the initial approach. With slight changes to the HTML file and CSS, The js file was revamped using the new knowledge on grid layout, further understanding of how event listeners work and also a better understanding on DOM manipulation. Indeed there is excitement in whatever level of progress one makes in learning programming

--- sketch.js
+++ sketch.js
@@ -1,14 +1,51 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.grid'); //Make a variable of the container
+const output = createElement(container, 'div', '', 'output');
+const control = document.querySelectorAll('.control');
+const resetBtn = document.querySelector('.reset')
+let pixVal = 10;
 
+//BUTTON OPERATIONS SECTION
 
-for (i = 0; i < 870 - 29; i++) {
-    const div = document.createElement('div');
-    div.classList.add('pad');
+control.forEach((button)=>{ //Using forEach method to gain access to the buttons
+    button.addEventListener('click', (e)=>{ //event listener with the event parameter
+        pixVal = e.target.textContent; // we use target to gain access  to the button's text content
+        return creatGrid(); //we return the creatGrid function to create the specified grid
+    });
+});
 
-    container.appendChild(div);
+resetBtn.addEventListener('click', reset); //Setting up the reset button
 
-    div.addEventListener('mouseup', ()=> {
-        div.style.backgroundColor = "black";
-    })
+/*****************************/
+
+//FUNCTIONS SECTION
+
+//CREATE ELEMENT FUNCTION
+function createElement(parent, eleType, html, eleClass) { //function for creating elements
+    const ele = document.createElement(eleType); //variable for element type
+    ele.inneHTML = html; //For whatever we want the innerHTML content to be
+    ele.classList.add(eleClass); //for creating  the element's class
+    return parent.appendChild(ele); //Now the new element can be rendered in the browser through the parent element
 }
+
+//FUNCTION FOR CREATING GRID
+function creatGrid() {
+    let total = pixVal ** 2; // the sqare of pixVal being required to create a grid with equal sides
+    for (i=0; i < total; i++) { //we use for loop to create the grids required in the container
+        const temp = createElement(output, 'div', '', 'box'); //while utilizing the createElement function
+        //the new div is output, while its class is box;
+    }
+    output.style.setProperty(`grid-template-columns`, `repeat(${pixVal}, 1fr)`); //we use grid template to create the boxes
+}
+
+//RESET FUNCTION
+function reset() { //we create a function
+    while (output.firstChild) { //using the while loop, we remove all content within the container'
+        output.removeChild(output.lastChild)
+    }
+    pixVal = 1;
+    creatGrid(); // then we replace it with single cell grid
+}
+/********************************************/
+
+
 
